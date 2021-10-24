b599c85cf91d14ea7c327e66b8e0426f4c0185b7
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-10-24 11:36:58 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -72,39 +72,3 @@ async def welcomey(bot,message):
                                                                    InlineKeyboardButton("Tech Support", url="t.me/redbullfed")],
                                                                   [InlineKeyboardButton("Music World", url="t.me/mwkmusics"),
                                                                    InlineKeyboardButton("Chat Group", url="t.me/shamilnelIi")]]))
-	
-
-@Bot.on_message((filters.group) & filters.new_chat_members)
-async def auto_welcome(bot: Bot, msg: Message):
-    first = msg.from_user.first_name
-    last = msg.from_user.last_name
-    mention = msg.from_user.mention
-    username = msg.from_user.username
-    id = msg.from_user.id
-    group_name = msg.chat.title
-    group_username = msg.chat.username
-    welcome_text = f"Hey {mention}\nWelcome To {group_name}"
-    print("Welcome Message Activate")
-    BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
-    if not BUTTON:
-       await msg.reply_text(text=WELCOME_TEXT.format(
-           first = msg.from_user.first_name,
-           last = msg.from_user.last_name,
-           username = None if not msg.from_user.username else '@' + msg.from_user.username,
-           mention = msg.from_user.mention,
-           id = msg.from_user.id,
-           group_name = msg.chat.title,
-           group_username = None if not msg.chat.username else '@' + msg.chat.username
-          )
-       )
-    else:
-       await msg.reply_text(text=WELCOME_TEXT.format(
-           first = msg.from_user.first_name,
-           last = msg.from_user.last_name,
-           username = None if not msg.from_user.username else '@' + msg.from_user.username,
-           mention = msg.from_user.mention,
-           id = msg.from_user.id,
-           group_name = msg.chat.title,
-           group_username = None if not msg.chat.username else '@' + msg.chat.username
-          ),
-       
