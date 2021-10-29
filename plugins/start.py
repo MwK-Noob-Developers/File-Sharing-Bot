8097a6e64a7769bd1b4617440fd8debc387840e4
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-10-29 20:02:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -117,10 +117,3 @@ async def not_joined(client: Client, message: Message):
         quote = True,
         disable_web_page_preview = True
     )
-
-@Bot.on_message(filters.regex("http") | filters.regex("www") | filters.regex("t.me") | filters.regex("ib") | filters.regex("pm") | filters.regex("dm") | filters.regex("fuck") | filters.regex("veno") | filters.regex("kitti") | filters.regex("sex") | filters.regex("vendevar"))
-async def nolink(bot,message):
-	try:
-		await message.delete()
-	except:
-		return
